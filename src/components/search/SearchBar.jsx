b2eb9898a04098021b2a50b3e0eb8021751ea9f2
rajeevrk9901 <rajeevrk9901@gmail.com>
2023-02-03 14:05:57 +0530

    removed absolute position

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -7,13 +7,7 @@ console.log(open)
     <div className='w-full '>
       <div className='bg-yellow-200 w-full h- flex items-center md:pl-48'>
         <div className='pl-24 py-6 flex gap-8 items-center'>
-        <button onClick={() => setOpen(!open)} className='md:flex hidden  '>
-            <OpenNav />
-          </button>
-          <input className='px-2 py-2' type="text" name="search" id="" placeholder='search' />
-          <button onClick={() => setOpen(!open)} className='flex md:hidden '>
-            <OpenNav />
-          </button>
+          <input className='px-2 py-2' type="text" name="search" id="" placeholder='Search' />
         </div>
       </div>
     </div>
@@ -23,15 +17,3 @@ console.log(open)
 export default SearchBar
 
 
-export const OpenNav = () => {
-  return (
-    <button className=''>
-      <ul className='flex flex-col gap-1'>
-        <li className='w-8 h-1 bg-black'></li>
-        <li className='w-6 h-1 bg-black'></li>
-        <li className='w-8 h-1 bg-black'></li>
-        <li className='w-6 h-1 bg-black'></li>
-      </ul>
-    </button>
-  )
-}
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -10,49 +10,41 @@ const Sidebar = () => {
 
     const [open, setOpen] = useState(false)
 
-    let navopenState = " absolute -left-[200px] "
+    let navopenState = "md:absolute  md:-left-[300px] "
 
-    let navcloseState = "  "
+    let navcloseState = "hidden"
 
     // console.log(open)
 
     return (
-        <div className='flex w-full absolute z-20'>
-
-            <div className={`fixed overflow-y-scroll h-full overflow-x-none  md:flex shadow-xl top-0 left-0 bg-red-200  flex flex-col gap-8 h-screen px-12 py-12 md:block ${open ?  navopenState : navcloseState}`}>
+        <div className='flex w-full absolute z-20 relative'>
+            
+            <div className={`fixed overflow-y-scroll h-full overflow-x-none  md:flex shadow-xl top-0 left-0 bg-red-200  flex flex-col gap-8 h-screen px-12 py-12 md:block ${open ? navopenState : navcloseState}`}>
                 <header className=' flex flex-col gap-2 justify-center w-full'>
+            
 
-                    {open ?
-                        <div className='hidden flex flex-col gap-2 justify-center'>
-                            <img width={40} src={logo} alt="logo-vietue" srcset="" />
-                            <p className='text-sm  text-center '>Gold</p>
-                        </div>
-                        :
-                        <div className='flex flex-col gap-2 justify-center'>
-                            <img width={100} src={logo} alt="logo-vietue" srcset="" />
-                            <p className='text-sm w-32'>Virtue Gold Pvt. Ltd.</p>
-                        </div>
-                    }
+                    <div className='flex flex-col gap-2 justify-center'>
+                        <img width={100} src={logo} alt="logo-vietue" srcset="" />
+                        <p className='text-sm w-32'>Virtue Gold Pvt. Ltd.</p>
+                    </div>
+
 
                 </header>
+                
                 <hr />
                 <div className='flex flex-col gap-3'>
-                    {open ? "" : <h1 className='uppercase font-bold text-gray-500'>Menu</h1>}
+                    <h1 className='uppercase font-bold text-gray-500'>Menu</h1>
 
                     <div className='flex flex-col gap-2'>
                         <ul className='flex items-center gap-4' >
-                            {Menu.map((data) => {
-                                return (
+                            <li onClick={() => setOpen(true)} className='flex gap-3 items-center' >
+                                <i className="  w-5"></i>
 
-                                    <li onClick={() => setOpen(true)} className='flex gap-3 items-center' >
-                                        <i className={`${data.icon} w-5  text-2xl hidden`}></i> 
-
-                                        {open ? "" : <h1>{data.name}</h1>}
-                                    </li>
+                                <h1>Dashboard</h1>
+                            </li>
 
 
-                                )
-                            })}
+
                         </ul>
                     </div>
                 </div>
@@ -60,16 +52,16 @@ const Sidebar = () => {
 
                 <div className='flex flex-col gap-3'>
 
-                    {open ? "" : <h1 className='uppercase font-bold text-gray-500'>Components</h1>}
+                    <h1 className='uppercase font-bold text-gray-500'>Components</h1>
 
                     <ul className='flex flex-col gap-4'>
 
                         {NavMainData.map((data) => {
                             return (
-                                <li  onClick={() => setOpen(true)} className='flex gap-3 items-center' >
-                                    {/* {open ? <i className={`${data.icon} w-5  text-2xl hidden`}></i> : <i className={`${data.icon} w-5`}></i>} */}
+                                <li onClick={() => setOpen(true)} className='flex gap-3 items-center' >
+                                    <i className={`${data?.icon} w-5`}></i>
 
-                                    {open ? "" : <NavLink to={`${data?.path}`}>{data.title}</NavLink>}
+                                    <NavLink to={`${data?.path}`}>{data.title}</NavLink>
 
                                 </li>
                             )
@@ -78,9 +70,11 @@ const Sidebar = () => {
                     </ul>
                 </div>
             </div>
+            <button onClick={() => setOpen(!open)} className={`md:hidden z-20 md:left-64 md:top-4 absolute ${open ? "left-64 top-4 ":"left-4 top-4  "}`}>
+               {!open ? <OpenNav /> : <CloseNav/> } 
+            </button>
 
 
-            <SearchBar open={open} setOpen={setOpen} />
         </div>
 
 
@@ -91,18 +85,29 @@ export default Sidebar
 
 
 
-
+export const OpenNav = () => {
+    return (
+        <button className=''>
+            <ul className='flex flex-col gap-1'>
+                <li className='w-8 h-1 bg-black'></li>
+                <li className='w-6 h-1 bg-black'></li>
+                <li className='w-8 h-1 bg-black'></li>
+                <li className='w-6 h-1 bg-black'></li>
+            </ul>
+        </button>
+    )
+}
 
 
 
 export const CloseNav = () => {
     return (
-        <div className='absolute right-0 -mr-20'>
+        <button className=''>
             <ul className='flex flex-col gap-1'>
                 <li className='w-8 absolute rotate-45 h-1 bg-black'></li>
                 <li className='w-8 absolute -rotate-45 h-1 bg-black'></li>
 
             </ul>
-        </div>
+        </button>
     )
 }
