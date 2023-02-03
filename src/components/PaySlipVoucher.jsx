7a1e44402d889263159781e8a906513f688195e1
Raushan Kumar <raushankp75@gmail.com>
2023-02-03 13:24:42 +0530

    Working on PaySlipVoucher Component

--- src/components/PaySlipVoucher.jsx
+++ src/components/PaySlipVoucher.jsx
@@ -2,21 +2,43 @@ import React from 'react'
 
 const PaySlipVoucher = () => {
     return (
-        // <div className=' absolute left-28'>
-        //     <div className='bg-gray-400 max-w-[500px]'>
-        //         <div className='flex flex-row justify-between'>
-        //             <h2>Virtue Gold pvt ltd</h2>
-        //             <h3>RECEIPT</h3>
-        //         </div>
-        //     </div>
-        // </div>
-
         <div className='absolute flex justify-center items-center md:mx-96 mx-5 md:my-16 my-5'>
-      <div className=' flex md:flex-row flex-col bg-blue-300 shadow-2xl'>
+      <div className=' flex md:flex-row flex-col bg-white shadow-2xl'>
         <div className='md:p-10 p-5 w-[800px]'>
-        <div className='flex flex-row justify-between'>
-            <h2 className='text-white text-3xl'>Virtue Gold pvt ltd</h2>
-            <h3>RECEIPT</h3>
+        <div className='flex flex-row justify-between '>
+            <h2 className='text-2xl font-bold'>Virtue Gold pvt ltd</h2>
+            <h3 className='text-xl font-semibold'>RECEIPT</h3>
+        </div>
+
+        <div >
+            <table border={2}>
+                <tr>
+                    <th className='px-7'>Item</th>
+                    <th className='px-7'>Description</th>
+                    <th className='px-7'>Quantity</th>
+                    <th className='px-7'>Weight</th>
+                    <th className='px-7'>Unit Price</th>
+                    <th className='px-7'>Amount</th>
+                    
+                </tr>
+                <tr>
+                    <td>Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                </tr>
+                <tr>
+                    <td>Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                </tr>
+               
+            </table>
         </div>
 
         </div>
