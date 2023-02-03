4ae606ec5d5d4adda25732686fab05b4dc7ee914
Raushan Kumar <raushankp75@gmail.com>
2023-02-03 14:07:29 +0530

    Working on PaySlipVoucher component modification

--- src/components/PaySlipVoucher.jsx
+++ src/components/PaySlipVoucher.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PaySlipVoucher = () => {
     return (
-        <div className='absolute flex justify-center items-center md:mx-96 mx-5 md:my-16 my-5'>
+        <div className=' flex justify-center items-center md:mx-96 mx-5 md:my-16 my-5 '>
       <div className=' flex md:flex-row flex-col bg-white shadow-2xl'>
         <div className='md:p-10 p-5 w-[800px]'>
         <div className='flex flex-row justify-between '>
@@ -11,34 +11,75 @@ const PaySlipVoucher = () => {
         </div>
 
         <div >
-            <table border={2}>
-                <tr>
-                    <th className='px-7'>Item</th>
-                    <th className='px-7'>Description</th>
-                    <th className='px-7'>Quantity</th>
-                    <th className='px-7'>Weight</th>
-                    <th className='px-7'>Unit Price</th>
-                    <th className='px-7'>Amount</th>
+            <table className='border'>
+                <tr className='border'>
+                    <th className='w-28 h-12 border-2 border-black'>Item</th>
+                    <th className='w-56 h-12 border-2 border-black'>Description</th>
+                    <th className='w-24 h-12 border-2 border-black'>Quantity</th>
+                    <th className='w-24 h-12 border-2 border-black'>Weight</th>
+                    <th className='w-24 h-12 border-2 border-black'>Unit Price</th>
+                    <th className='w-24 h-12 border-2 border-black'>Amount</th>
                     
                 </tr>
-                <tr>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
+                <tr className=''>
+                    <td className='w-28 h-12 border-2 border-black'>Gold</td>
+                    <td className='w-56 h-12 border-2 border-black'>This is unique Lorem ipsum dolor sit amet.</td>
+                    <td className='w-24 h-12 border-2 border-black'>2</td>
+                    <td className='w-24 h-12 border-2 border-black'>2kg</td>
+                    <td className='w-24 h-12 border-2 border-black'>1000</td>
+                    <td className='w-24 h-12 border-2 border-black'>2000</td>
                 </tr>
-                <tr>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
-                    <td>Item</td>
+                <tr className=''>
+                    <td className='w-28 h-12 border-2 border-black'></td>
+                    <td className='w-56 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
                 </tr>
-               
+                <tr className=''>
+                    <td className='w-28 h-12 border-2 border-black'></td>
+                    <td className='w-56 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                </tr>
+                <tr className=''>
+                    <td className='w-28 h-12 border-2 border-black'></td>
+                    <td className='w-56 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                </tr>
+                <tr className=''>
+                    <td className='w-28 h-12 border-2 border-black'></td>
+                    <td className='w-56 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                </tr>
+                <tr className=''>
+                    <td className='w-28 h-12 border-2 border-black'></td>
+                    <td className='w-56 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                </tr>
+                <tr className=''>
+                    <td className='w-28 h-12 border-2 border-black'></td>
+                    <td className='w-56 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                    <td className='w-24 h-12 border-2 border-black'></td>
+                </tr>
             </table>
+
+            <div></div>
         </div>
 
         </div>
