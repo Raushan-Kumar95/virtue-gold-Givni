52d2e05ce2afc0e06a5cf98d5d4aaea2b24bd83e
Raushan132 <raushankumar132abc@gmail.com>
2023-02-06 13:18:04 +0530

    change buy silver to buy coin

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -5,7 +5,7 @@ import BuySilver from "./coins/BuySilver";
 const Coins= ()=>{
     
 
-    const [product,setProduct]=useState([])
+    const [product,setProduct]=useState(["Gold","Buy"])
 
     const[active,setActive]=useState(0);
 
@@ -25,7 +25,7 @@ const Coins= ()=>{
                 <div className={active==0?"cursor-pointer border-b-2 border-b-red-600":"cursor-pointer"} onClick={()=>{setProduct(['Gold', "Buy"]); setActive(0)}} >Buy Gold</div>
                 <div className={active==1?"cursor-pointer border-b-2 border-b-red-600":"cursor-pointer"} onClick={()=>{setProduct(['Silver', "Buy"]); setActive(1)}}>Buy Silver</div>
                 <div className={active==2?"cursor-pointer border-b-2 border-b-red-600":"cursor-pointer"} onClick={()=>{setProduct(['Gold',"Sell"]); setActive(2)}}>Sell Gold</div>
-                <div className={active==3?"cursor-pointer border-b-2 border-b-red-600":"cursor-pointer"} onClick={()=>{setProduct(['Silver','Sell']); setActive(3)}}>Sell Gold</div>
+                <div className={active==3?"cursor-pointer border-b-2 border-b-red-600":"cursor-pointer"} onClick={()=>{setProduct(['Silver','Sell']); setActive(3)}}>Sell Silver</div>
             </div>
             <hr className=' ' />
         </div>
@@ -34,7 +34,7 @@ const Coins= ()=>{
 
 
         {/* <BuyCoin /> */}
-        <BuySilver p_name={product[0]} buy_sell={product[1]} />
+        <BuyCoin p_name={product[0]} buy_sell={product[1]} />
     </div>
 </>
     )
